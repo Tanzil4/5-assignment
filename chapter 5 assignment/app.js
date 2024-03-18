4650a0ee07b6f72127c7bb6827c9d90ba884e3dc
Tanzil-khan <155762084+Tanzil4@users.noreply.github.com>
2024-03-18 23:46:29 +0500

    Add files via upload

---
+++ chapter 5 assignment/app.js
@@ -0,0 +1,22 @@
+let str1 = "tanzil"
+let str2 = " khan"
+let str3 = str1.concat(str2)
+console.log(str3);
+
+
+
+
+
+
+  // Cost of one movie ticket is 600 PKR
+  let ticketPrice = 600;
+
+  // Number of tickets to buy
+  let numberOfTickets = 5;
+
+  // Calculate the total cost
+  let totalCost = ticketPrice * numberOfTickets;
+
+  // Output the result
+  console.log(`Cost of one movie ticket is  ${ ticketPrice } `);
+  console.log(`Cost of buying  ${numberOfTickets} tickets to a movie is ${ totalCost} `);
